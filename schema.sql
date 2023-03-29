b552343847fed8f348ae3a562703d99fea94d1f3
theaveragesiren <theaveragesiren@gmail.com>
2023-03-29 23:02:48 +0500

    Fix

--- schema.sql
+++ schema.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS project
 (
     project_id int NOT NULL PRIMARY KEY  AUTO_INCREMENT,
     project_name varchar(255) NOT NULL,
-    creator_id int NOT NULL,
     FOREIGN KEY (creator_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS task 
@@ -10,9 +9,9 @@ CREATE TABLE IF NOT EXISTS task
     task_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     task_date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     task_status int DEFAULT 0,
-    task_name varchar(255) NOT NULL,
+    task_title varchar(255) NOT NULL,
     task_file varchar(255),
-    task_date_finish varchar(10),
+    task_date_completed varchar(10),
     task_project_id  int NOT NULL,
     FOREIGN KEY (task_project_id) REFERENCES project(project_id) on DELETE CASCADE
 );
